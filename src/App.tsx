2d260c18a490af1abd10b46415db50ff4d2d61cd
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2022-10-03 22:19:31 +0700

    fix bug socket

--- src/App.tsx
+++ src/App.tsx
@@ -11,74 +11,76 @@ import { useAppDispatch, useAppSelector } from "./redux/hook";
 // import tokenService from "./services/token.service";
 import userAPI from "./redux/user/userAPI";
 // import io from "socket.io-client";
-import io, {Socket} from "socket.io-client";
+import io, { Socket } from "socket.io-client";
 import roomAPI from "./redux/Room/roomAPI";
-import {useSocket} from "./common/configSocket"
+import { useSocket } from "./common/configSocket";
 
-let socket:Socket;
+let socket: Socket;
 export const sendMessageSocket = (
-    roomId: any,
-    content: any,
-    type: any,
-    token: any
+  roomId: any,
+  content: any,
+  type: any,
+  token: any
 ) => {
-    console.log("fincuon chat======================");
-
-    socket.emit("client-send-message", { token, roomId, content, type });
+  socket.emit("client-send-message", { token, roomId, content, type });
 };
 
-console.log("=========================================");
-
 function App() {
-    const userState = useAppSelector((state: any) => state.user);
-    const dispatch = useAppDispatch();
-    const token = userState.accessToken;
-    console.log({ token });
+  const userState = useAppSelector((state: any) => state.user);
+  const dispatch = useAppDispatch();
+  const token = userState.accessToken;
 
-   const socket = useSocket();
+  useEffect(() => {
+    console.log({ token: token });
+    if (!token) {
+      console.log("-----------error token");
+      return;
+    } else {
+      console.log("-----------Ok token");
+    }
+    const socket = io("http://localhost:5000", {
+      query: {
+        token: token,
+      },
+    });
+    socket?.on("server-send-message", function (data) {
+      console.log("-----------------------------------==========");
+      console.log({ data });
+      dispatch(roomAPI.updateListChat()(data));
+    });
+    socket?.on("connect", () => {
+      //   setIsConnected(true);
+      console.log("connecting");
+    });
+    socket?.on("disconnect", () => {
+      //   setIsConnected(false);
+      console.log("disconnect");
+    });
+    return () => {
+      socket?.off("connect");
+      socket?.off("disconnect");
+      socket?.off("server-send-message");
+    };
+  }, [token]);
 
-    useEffect(() => {
-        socket?.on("server-send-message", function(data)  {
-            console.log("======dfdsfdsf dfsddgfdg====");
-            console.log(data);
-
-            dispatch(roomAPI.updateListChat()(data));
-            console.log("=======dfdfsdfds==============");
-
-        });
-        // socket?.on("connect", () => {
-        //     //   setIsConnected(true);
-        //     console.log("connecting");
-        // });
-        // socket?.on("disconnect", () => {
-        //     //   setIsConnected(false);
-        //     console.log("disconnect");
-        // });
-        return () => {
-            socket?.off("connect");
-            socket?.off("disconnect");
-            socket?.off("server-send-message");
-        };
-    }, []);
-
-    return (
-        <div className="App">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <ControlBar />
-                            <FriendTag />
-                            <Content />
-                        </>
-                    }
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <ControlBar />
+              <FriendTag />
+              <Content />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
